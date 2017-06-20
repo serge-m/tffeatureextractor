83cd99669d1543100cd7697630f94908733e0bde
Sergey Matuynin <sbmatyunin@gmail.com>
2017-06-20 22:43:26 +0200

    added draft for communication with tensorflow serving

--- tffeatureextractor/feature_extractor.py
+++ tffeatureextractor/feature_extractor.py
@@ -1,12 +1,26 @@
+from __future__ import print_function
+
+from grpc.beta import implementations
+import tensorflow as tf
+
+from tensorflow_serving.apis import predict_pb2, prediction_service_pb2
+
 import numpy as np
 
-#from tffeatureextractor_detector import DlibFaceDetector
 from kolasimagesearch.impl.feature_engine.feature_extractor import FeatureExtractor, Descriptor
+
+
+class TensorflowProxy(object):
+    def get_desctiptor(self, image_encoded):
+        return Descriptor([0])
+
+
+def encode_image(image):
+    return image
 
 
 class TFFeatureExtractor(FeatureExtractor):
     def __init__(self, *args, **kwargs):
-        super(self, TFFeatureExtractor).__init__(*args, **kwargs)
         self.tensorflow_proxy = TensorflowProxy()
 
     def calculate(self, image: np.ndarray) -> Descriptor:
@@ -14,3 +28,30 @@ class TFFeatureExtractor(FeatureExtractor):
         feature_vector = self.tensorflow_proxy.get_desctiptor(image_encoded)
         return Descriptor(feature_vector)
 
+
+class TFConnection:
+    def __init__(self, server_url_with_port="localhost:9000"):
+        host, port = server_url_with_port.split(':')
+        channel = implementations.insecure_channel(host, int(port))
+        self.stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
+        self.timeout = 10.0
+
+    def predict(self, bin_image):
+        request = predict_pb2.PredictRequest()
+        request.model_spec.name = 'inception'
+        request.model_spec.signature_name = 'predict_images'
+        request.inputs['images'].CopyFrom(tf.contrib.util.make_tensor_proto(bin_image, shape=[1]))
+        return self.stub.Predict(request, self.timeout)
+
+
+def main():
+    path_image = "./test.jpg"
+    with open(path_image, 'rb') as f:
+        data = f.read()
+
+    tf_connection = TFConnection()
+    print(tf_connection.predict(data))
+
+
+if __name__ == '__main__':
+    main()
